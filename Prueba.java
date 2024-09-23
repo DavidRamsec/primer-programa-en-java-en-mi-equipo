10eedc3e878ef08b27ee39166314e5456c18f24f
DavidRamsec <davidramswezz@gmail.com>
2024-09-23 13:26:24 +0200

    Ya no muestra la línea de los apellidos y aparace una linea con el grupo musical favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Ramos Seca");
 		System.out.println("18 años, 2005");
 		System.out.println("Arroz");
+		System.out.println("Bring me the horizon");
 		
 	}
 	
